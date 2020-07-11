a56e247be6e9b0ce4057411b49083fed1d6dc73b
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-11 21:26:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,5 @@
 def speak_to_grandma(phrase)
+<<<<<<< HEAD
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
@@ -10,4 +11,18 @@ end
 
 
 
+=======
+  
+  if phrase=="I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase==phrase.upcase
+    puts "HUH!? SPEAK UP, SONY!"
+  elsif phrase==phrase.downcase
+  puts "HUH!? SPEAK UP, SONY!"
+else
+  puts "NO, NOT SINCE 1938!"
+end
+
+end
+>>>>>>> 8b6cec22607e5832a59eab8f16f28a083d43b58f
     
